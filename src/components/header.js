c5545db692d118daea775802b99f634dcb523a0f
Kara Diaby <kdiaby.pro@gmail.com>
2017-03-14 17:00:50 +0100

    some changes

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return(
    <div className="text-center">
       <h3>SPA Youtube ajax search by Kara Diaby</h3>
-      <p>Created by React JS and Redux. React is very powerful. Fell free to download the source code</p>
+      <p>Created by React JS and Redux. React is very powerful. Feel free to download the source code</p>
    </div>
   );
 
